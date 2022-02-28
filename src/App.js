4c42611959d23cecc583f7c7ffd7cf8af3402836
yadavsahil <yadav.sahil52@gmail.com>
2022-02-28 08:07:45 +0530

    crypto_tracker

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route} from "react-router-dom";
+import './App.css';
+import Header from "./components/Header";
+import CoinPage from "./Pages/CoinPage";
+import Homepage from "./Pages/Homepage";
+import {makeStyles}from "@material-ui/core";
+function App() {
+  const useStyles = makeStyles(() => ({
+    App:{
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight:'100vh',
+    },
+  }));
+
+  const classes = useStyles()
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path='/' component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} />
+        
+      </div>
+    </BrowserRouter>
+    
+  )
+}
+
+export default App;
